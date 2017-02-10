e8c92b531ebebbf59659926aa354460d0d08d4a7
SLI417 <SLI417@student.glendale.edu>
2017-02-10 15:58:14 -0800

    Add files via upload

---
+++ SortingAlgorithm.java
@@ -0,0 +1,23 @@
+
+public class SortingAlgorithm{
+
+	
+	public int[] selectiveSortIterative (int[] array){
+		for (int i = 0; i < array.length; i++){
+			
+		}
+	}
+	
+	public int[] selectiveSortRecursion (int[] array){
+		
+	}
+	
+	public int[] insertionSortIterative (int[] array){
+		int pos, temp;
+		for (int i = 1; i < array.length; i++){
+			
+		}
+		return array;
+	}
+
+}
